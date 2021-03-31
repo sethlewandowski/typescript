18bde68595c0b583be8bf6bd42b97363c776676b
Seth Lewandowski <sethlew@protonmail.com>
2021-03-31 15:21:36 -0700

    playing with ts

---
+++ types.js
@@ -0,0 +1,7 @@
+var myString;
+var myNum;
+var myBool;
+myString = 'Im seth';
+myNum = 22;
+myBool = true;
+console.log(myString);
---
+++ types.ts
@@ -0,0 +1,9 @@
+let myString: string
+let myNum: number
+let myBool: boolean
+
+myString = 'Im seth'
+myNum = 22
+myBool = true
+
+console.log(myString)
